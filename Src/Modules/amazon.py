b0dfec50e071c0838fed3e8622a6dfd73904e900
= <=>
2025-12-07 23:15:37 +0200

    I am losing my mind , please kill me ( oh and also made amazon.py finally work with the nis->usd conversion)

--- Src/Modules/amazon.py
+++ Src/Modules/amazon.py
@@ -1,71 +1,90 @@
 import sys
-sys.dont_write_bytecode = True # Fuck Byte code , Python.
+sys.dont_write_bytecode = True
 
 import cloudscraper
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from time import sleep
+import json
 
-def parse(text:str, left:str, right:str) -> str:
+
+def parse(text: str, left: str, right: str) -> str:
     start = text.index(left) + len(left)
-    end = text.index(right , start)
+    end = text.index(right, start)
     return text[start:end]
 
 
+def scrape_amazon():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    CHROME_PATH = os.path.join(BASE_DIR, "..", "..", "ChromeDriver", "chromedriver.exe")
+    service = Service(CHROME_PATH)
+    driver = webdriver.Chrome(service=service)
+    driver.get("https://amazon.com")
+    sleep(2)
+    #open the prefrence page, gotta change the NIS->USD
+    driver.get("https://www.amazon.com/customer-preferences/edit/")
+    sleep(2)
 
-def get_average_price(Id:str) -> int:
-    
+    wait = WebDriverWait(driver, 10) # max 10 seconds before Quiting!
+
+    dropdown = wait.until(EC.element_to_be_clickable(
+        (By.ID, "icp-currency-dropdown-selected-item-prompt")
+    ))
+    dropdown.click()
+
+    usd_option = wait.until(EC.element_to_be_clickable(
+        (By.ID, "icp-currency-dropdown_1")  # USD
+    ))
+    usd_option.click()
+
+    save_button = wait.until(EC.element_to_be_clickable(
+        (By.ID, "icp-save-button")
+    ))
+    save_button.click()
+
+
+
+    # STEP 5 — Now perform search
+    Search = input("enter what you would wish to search: ")
+    search_url = f"https://www.amazon.com/s?k={Search.replace(' ', '+')}"
+    driver.get(search_url)
+
+    print("Search loaded successfully in USD mode.")
+    sleep(100)
+
+
+def get_average_price(Id: str) -> int:
     scraper = cloudscraper.create_scraper()
 
     headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0"
     }
 
-
-    item = "B0FQNB9WBD"
     url = f"https://camelcamelcamel.com/product/{Id}"
-    print(f"=================================      URL    ====================================\n\t{url}")
-    print(f"==================================================================================\n\n\n\n")    
-    # Making the request to the camelcamelcamel api , tells average prices which we can compare later.
+    print(f"URL: {url}")
+
     response = scraper.get(url, headers=headers)
-    print(f"===================================== STATUS CODE ================================\n\t\t\t\t\t{response.status_code}")
-    print(f"==================================================================================")
-    html = BeautifulSoup(response.text , 'html.parser')
+    print("Status Code:", response.status_code)
 
-
+    html = BeautifulSoup(response.text, 'html.parser')
     parent_prices = html.find(class_="pt amazon on")
-
 
     all_prices = parent_prices.find_all('td')
 
+    prices_arr = [p.text.strip() for p in all_prices]
 
-    prices_arr = []
+    print("Average USD Price:", prices_arr[4])
+    return prices_arr[4]
 
-    for price in all_prices:
-        prices_arr.append(price)
-
-    print(prices_arr[4].text.strip())
 
 def main():
+    scrape_amazon()
 
-    try:
-        url = input("Enter the url: ")
-        ID = parse(url, 'dp/', '/')
-    except Exception as ValueError:
-        ID = parse(url, 'dp/' , '?')
-    a = get_average_price(ID)
-    
 
 if __name__ == "__main__":
     main()
-"""
-url ref:
-class="a-link-normal s-no-outline"
-
-title:
-class="a-size-medium a-spacing-none a-color-base a-text-normal"
-
-price:
-class="a-price-whole"
-
-"""
