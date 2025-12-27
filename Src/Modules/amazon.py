605fc9cc18e7fc7f90efa2a021b89ea7a387b95c
= <=>
2025-12-27 21:01:37 +0200

    Refactor code structure for improved readability and maintainability. Updated amazon.py

--- Src/Modules/amazon.py
+++ Src/Modules/amazon.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 import os
 from time import sleep
 import json
@@ -17,6 +18,16 @@ def parse(text: str, left: str, right: str) -> str:
     start = text.index(left) + len(left)
     end = text.index(right, start)
     return text[start:end]
+
+def scrape_listings(soup):
+    listings = soup.find_all('div',class_='sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20 gsx-ies-anchor')
+    for listing in listings:
+        price_span = listing.find('span', 'a-price-whole')
+        if price_span:
+            price = price_span.contents[0].strip()
+            print(price)
+        elif listing.find('span',class_="a-size-base a-color-secondary"):
+            continue
 
 
 def scrape_amazon():
@@ -49,13 +60,37 @@ def scrape_amazon():
 
 
 
-    # STEP 5 — Now perform search
+    # Now perform the search , AFTER WE NEED TO FUCKING GET A SHIT TON OF ELEMENTS JUST FOR FUCKASS NIS->USD
     Search = input("enter what you would wish to search: ")
     search_url = f"https://www.amazon.com/s?k={Search.replace(' ', '+')}"
     driver.get(search_url)
 
-    print("Search loaded successfully in USD mode.")
+    print("[DEBUG]: Search loaded successfully in USD mode.")
+    html = driver.page_source
+    soup = BeautifulSoup(html , 'html.parser')
+    while True:
+        #Update soup for the current page
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        scrape_listings(soup)
+        # get the next page element in amazon , we access it by css selector
+        next_button_selector = ".s-pagination-next"  
+        next_page_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, next_button_selector))) # search for the element , and wait for it to be avaliable
+        string_page_btn = soup.select(next_button_selector)
+        
+        if "s-pagination-disabled" in str(string_page_btn):
+            print(f"[DEBUG]: no more pages to scrape, exiting!")
+            driver.quit()
+            exit()
+
+        else:    
+            print(f"[DEBUG]: Navigating to next page...")
+            next_page_btn.click()
+        
+        sleep(2)
+
+
     sleep(100)
+
 
 
 def get_average_price(Id: str) -> int:
